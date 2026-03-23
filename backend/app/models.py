6e3ff4fea49776ae805843122fcc1ede707d6b42
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-03-23 19:27:48 +0530

    Category & Product Model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    icon = models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=500)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=4.0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
